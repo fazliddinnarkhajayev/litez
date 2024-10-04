362ec7bc20d14c58d7bd1e0ec4e718b9f690b0ba
fazliddin <fazliddinnarkhajayev@gmail.com>
2024-10-04 18:59:41 +0500

    add

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+    const sidebar = document.querySelector('.menu');
+    sidebar.classList.toggle('close');
+});
+
+// document.getElementById('toggleButton').addEventListener('click', function() {
+//     const sidebar = document.querySelector('.sidebar');
+//     sidebar.classList.toggle('active');
+// });
